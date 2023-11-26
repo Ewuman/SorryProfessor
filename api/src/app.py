689ae8e0ae6ed0f929cf8236f03ecb9cced88f77
Ian Dai <ianbbdai@gmail.com>
2023-11-25 18:26:47 -0800

    Changed app to receive form data

--- api/src/app.py
+++ api/src/app.py
@@ -1,29 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from text_generation import TextGenerator
 import os
 import sqlite3
 from collections import namedtuple
+import json
 
 # provide your own api key
 PALM_API_KEY = 'AIzaSyBYZnbTdqa-rByKbOFWPMF10ZveCxdKDJA'
 DB_FILENAME = "excuses.db"
-DB_PATH = os.path.join("..", "db", DB_FILENAME) # path should work on any OS
+DB_PATH = os.path.join("db", DB_FILENAME) # path should work on any OS
 
 app = FastAPI()
 
 # create data model for items
 Item = namedtuple('Item', ['professor', 'excuse', 'assignment'])
 
-def insert_data(item: Item):
+def insert_data(item):
     # insert data into db
-    conn = sqlite3.connect(DB_PATH) # implicitly creates db if it doesn't exist already 
+    conn = sqlite3.connect(DB_FILENAME) # implicitly creates db if it doesn't exist already 
     cursor = conn.cursor()
 
     # create table if it doesn't exist
     cursor.execute("CREATE TABLE IF NOT EXISTS Record(professor TEXT PRIMARY KEY, excuse TEXT, assignment TEXT);")
 
     # check if current professor in db
-    res = list(cursor.execute("SELECT professor FROM Record WHERE professor = (?);", (item.professor,)))
+    res = list(cursor.execute("SELECT professor FROM Record WHERE professor = (?);", (item["professor"],)))
     
     if len(res) != 0:
         conn.commit()
@@ -31,7 +32,7 @@ def insert_data(item: Item):
         return False
 
     cursor.execute("""INSERT INTO Record (professor, excuse, assignment)
-                    VALUES (?, ?, ?);""", (item.professor, item.excuse, item.assignment))
+                    VALUES (?, ?, ?);""", (item["professor"], item["excuse"], item["assignment"]))
     
     conn.commit()
     conn.close()
@@ -50,8 +51,10 @@ def on_shutdown() -> None:
     pass
 
 
-@app.post("/placeholder") # change this to proper route
-def get_text(item: Item):
+@app.post("/generate") # change this to proper route
+def get_text(prof: str=Form(), excuse: str=Form(), assgn: str=Form()):
+    
+    item = {"professor": prof, "excuse": excuse, "assignment": assgn}
     # set parameters for PALM generation and update db
     
     # update db
@@ -62,7 +65,7 @@ def get_text(item: Item):
 
     # generate text
     text_gen = TextGenerator(PALM_API_KEY)
-    text_gen.set_prompt(item.assignment, item.excuse)
+    text_gen.set_prompt(item["assignment"], item["excuse"])
     res = text_gen.generate_text()
 
     return {"message" : res}
